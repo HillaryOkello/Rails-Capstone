7917f2e2c8db982712a06055614bcaa6d3a3d6e1
HillaryOkello <hillariouskelly@outlook.com>
2021-03-05 13:20:34 +0300

    setup bootstrap

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.0'
 gem 'rubocop'
 
+gem 'bootstrap', '~> 4.0.0'
+gem 'jquery-rails'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3'
 # Use sqlite3 as the database for Active Record
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "../stylesheets/application"
+
+//= require jquery3
+//= require popper
+//= require bootstrap
 
 Rails.start()
 Turbolinks.start()
