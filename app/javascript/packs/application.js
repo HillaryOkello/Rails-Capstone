92879062553324b54bb7aed2cbc1b929c053ff50
HillaryOkello <hillariouskelly@outlook.com>
2021-03-13 19:52:53 +0300

    minor change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
