dbd37d7816bc4ad15ac9785141962fcc7bf6a755
HillaryOkello <hillariouskelly@outlook.com>
2021-03-13 19:46:03 +0300

    remove stylesheets import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "../stylesheets/application"
+// import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
