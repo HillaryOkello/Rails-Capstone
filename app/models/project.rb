b6818a7d53a90771af5c0e8e4e8f3405fc38c97c
HillaryOkello <hillariouskelly@outlook.com>
2021-03-15 00:35:55 +0300

    add User model test

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,7 @@ class Project < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 30 }
   validates :hours, presence: true
+  validates :category, presence:true
 
   default_scope { order(created_at: :desc) }
 end
